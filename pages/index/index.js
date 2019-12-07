c4c9b24f35d4a98fe1e5a56275bb9ab06acf71c3
unknown <414595969@qq.com>
2019-12-07 11:59:30 +0800

    静态首页完成

---
+++ pages/index/index.js
@@ -0,0 +1,76 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    messages:"热烈庆祝校园校服商城开始开发！",
+    // 轮播图数据
+    images:["../../images/1.jpg","../../images/2.jpg","../../images/3.jpg"],
+    // 商品图片数据
+    bigImg:["../../images/big1.jpg","../../images/big2.jpg"],
+    // 商品数据
+    showGoods:[
+      {img:'../../images/1.jpg',title:'秋冬衣服',money:'3.00',old:'10.00',id:1},
+      {img:'../../images/2.jpg',title:'秋冬衣服',money:'5.00',old:'10.00',id:2},
+      {img:'../../images/3.jpg',title:'秋冬衣服',money:'6.00',old:'10.00',id:3}
+    ],
+    map:'福建省泉州市丰泽区...（点击跳转地图）'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
